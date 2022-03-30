18fcac0ae71f6b191bbe4bce1034d4a910449bc2
Rishabbh <69572197+Rishabbh-Sahu@users.noreply.github.com>
2022-03-30 20:10:51 +0100

    updated PEP 257 Docstring Conventions

--- text_preprocessing/preprocessing.py
+++ text_preprocessing/preprocessing.py
@@ -13,11 +13,11 @@ def remove_next_line(txt_arr):
     return [sub.replace('\n', '') for sub in txt_arr]
 
 def remove_punctuations(txt_arr):
-    '''
+    """
     data cleansing in order to remove various punctuations exits in the file
     txt_arr: list [] - [[sequence of text1],[sequence of text2]...]
     return: cleaned text without the specified punctuations
-    '''
+    """
     return txt_arr
 
 
